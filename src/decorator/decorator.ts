b93c08ec79604b8e5c6943edbf5f19190baed88d
Jinguo.Yin <jinguo.yin@me.com>
2023-12-06 23:27:50 +0800

    modified:   src/decorator/decorator.ts

--- src/decorator/decorator.ts
+++ src/decorator/decorator.ts
@@ -1,33 +1,23 @@
 import 'reflect-metadata';
 
-export const PARAMTYPES_METADATA = 'design:paramtypes';
-export const INJECTABLE = 'injectable';
-export const INJECT_NAME = 'inject:name';
-export const INJECT_TYPE = {
-    CLASS: 'class',
-    CUSTOM: 'custom',
-    WINDOW: 'window'
-};
-
 export const METHOD_HANDLE = "method:handle";
 export const PROPERTY_HANDLE = 'property:handle';
 export const CLASS_HANDLE = 'class:handle';
 
-export function Controller(controllerName: string): ClassDecorator {
+export function Controller(controllerName: string): ClassDecorator {    //Class Decorator
     return (_) => {
         console.log('_', _, controllerName);
         Reflect.defineMetadata(CLASS_HANDLE, controllerName, _);
     }
 }
 
-
-export function MethodDec(componentName: string): MethodDecorator {        //函数组件装饰器
+export function MethodDec(componentName: string): MethodDecorator {        //Functional Component Decorator
     return (target, propertyName) => {
         Reflect.defineMetadata(METHOD_HANDLE, componentName, target, propertyName);
     }
 }
 
-export function PropertyDec(propertyName: string): PropertyDecorator {
+export function PropertyDec(propertyName: string): PropertyDecorator {  //Property Decorator
     if(!propertyName) {
         throw new Error('inject name is required');
     }
