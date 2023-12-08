6c3e853cabedfc1494a01e99eeb1a587e4a93fe9
Jinguo.Yin <jinguo.yin@me.com>
2023-12-08 21:48:33 +0800

    support dynamic stringify javascript code and parse 	modified:   package.json 	modified:   public/index.html 	modified:   src/MyTest.tsx 	modified:   yarn.lock

--- src/MyTest.tsx
+++ src/MyTest.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable no-new-func */
 import React from 'react'
+import * as acorn from 'acorn';
 
 export interface MyTestProps {
   name?: string;
@@ -6,20 +8,41 @@ export interface MyTestProps {
 }
 
 const MyTest: React.FC<MyTestProps> = (props) => {
-
-  const display = () => {
-    alert('this is from json');
+  const display = (p: string) => {
+    alert(`this is from json ${p}`);
   }
   const handleClick = () => {
-    console.log('props.onClick', props.action);
-    if(props.action) {
-      eval(props.action);      
+    const hello = () => {
+      console.log('hello from MyTest');
     }
+
+    const str =  `
+      const {hello, display} = arguments[0]; 
+      const {hello: envHello, dispay: envDisplay} = env;
+      console.log("env is: ", env);
+      hello(); 
+      return 1;
+    `;
+
+    let script = null; 
+    try{
+      script = acorn.parse(str,  {ecmaVersion: 'latest', allowReturnOutsideFunction: true});
+    }catch (err: any) {
+      console.log('err: ', err);
+    }
+
+    console.log('script: ', script);
+
+    const func = new Function('env',str);
+    const result = func({hello: hello, display: display});
+    console.log('result: ', result);
   }
   
   return (
     <div>This is MyTest {props?.name}
-      <button onClick={handleClick}>Click</button>
+      <button onClick={handleClick}>TestString</button>
+      <div dangerouslySetInnerHTML={{__html: '<div>This is from dangerouslySetInnerHTML1</div>'}} />
+      <input type = 'text'></input>
     </div>
   )
 }
